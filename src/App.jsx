68390221db5da3c5c9ec7bb5b4bc9275cdc22916
Pratik Wadhai <pratikwadhai001@gmail.com>
2024-08-31 12:19:22 +0530

    added svg and changed button

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import Result from "./components/Result";
 
 export default function App() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-[#f0f7ff] p-4">
-      <div className="bg-white rounded-3xl shadow-xl flex max-w-4xl w-full overflow-hidden">
+    <div className="flex items-center justify-center min-h-screen w-full bg-[#f0f7ff] p-4">
+      <div className="bg-white h-full rounded-3xl shadow-xl flex max-w-4xl w-full overflow-hidden">
         <Calculator />
         <Result />
       </div>
